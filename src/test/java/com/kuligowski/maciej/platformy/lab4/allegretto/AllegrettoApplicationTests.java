2219a0437c6ea792bce216287c59b59e16f18381
farfax <mfarfax@gmail.com>
2018-04-07 15:25:04 +0200

    Initial commit, set project structure, swagger, jetty

---
+++ src/test/java/com/kuligowski/maciej/platformy/lab4/allegretto/AllegrettoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuligowski.maciej.platformy.lab4.allegretto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AllegrettoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
